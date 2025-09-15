fb70ed8aa415e951be41ce8767044eeea6adff2f
Mounika-guru39 <gurumounika29@gmail.com>
2025-09-15 18:45:00 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import Dashboard from './pages/Dashboard';
+import UserDetails from './pages/UserDetails';
+import './App.css';
+
+function App() {
+  return (
+    <UserProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/user/:id" element={<UserDetails />} />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
+  );
+}
+
+export default App;
